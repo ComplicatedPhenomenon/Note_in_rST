19e70b563775dc70cf69755c21657db767a2c347
ComplicatedPhenomenon <czfzdxx@outlook.com>
2018-05-23 23:26:37 +0800

    Delete output & customize theme

--- source/conf.py
+++ source/conf.py
@@ -82,14 +82,22 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+# alabaster
+# classic
+# scrolls
+# traditional
+# ...
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+html_theme_options = {
+"rightsidebar" : True,
+"stickysidebar" : True, 
+"collapsiblesidebar" : True,
+"bodyfont" : 'serif'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
